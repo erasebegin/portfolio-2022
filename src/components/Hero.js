5b2be29a267c7aceba19d4e84d5b00cfc5770695
nexus-chris <chris.haupt@nexuspoint.co.uk>
2022-07-16 21:13:46 +0100

    hero v2 complete

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,6 +9,7 @@ import {
   Flex,
   Heading,
   Image,
+  Spacer,
   Stack,
   Text,
 } from '@chakra-ui/react';
@@ -21,10 +22,12 @@ export default function Hero({ openContact, setOpenContact }) {
   const transition = time => `${time || '400ms'} ease-in-out`;
   console.log(transition());
   return (
-    <Box>
+    <Box minH="800px">
       <Divider color={theme.colors.blue[100]} />
-
-      <Stack pt={[100, 100, 100]}>
+      <Stack
+        pt={[showLogos ? 15 : 0, showLogos ? 15 : 0, showLogos ? 150 : 50]}
+        transition="400ms ease-in-out"
+      >
         <Flex
           align="center"
           justify="center"
@@ -38,8 +41,8 @@ export default function Hero({ openContact, setOpenContact }) {
             pt={[3, 3, 1]}
             pb={1}
             transform={[
-              showLogos ? 'translateY(100%)' : 'translateY(35%)',
-              showLogos ? 'translateY(100%)' : 'translateY(35%)',
+              showLogos ? 'translateY(103%)' : 'translateY(35%)',
+              showLogos ? 'translateY(103%)' : 'translateY(35%)',
               showLogos ? 'translateX(100%)' : 'translateX(2rem)',
             ]}
             zIndex="3"
@@ -66,11 +69,12 @@ export default function Hero({ openContact, setOpenContact }) {
           <Button
             onClick={() => setShowLogos(false)}
             pos="absolute"
-            left="48%"
-            top={['3rem', showLogos ? '-2rem' : '0.5rem']}
-            h={20}
-            colorScheme="cyan"
+            left={['80%', '80%', '48%']}
+            top={[0, 0, showLogos ? '-2rem' : '0.5rem']}
+            h={[10, 10, 20]}
+            colorScheme="blue"
             transition="400ms ease-in-out"
+            opacity={[showLogos ? 1 : 0, showLogos ? 1 : 0, 1]}
           >
             <Stack justify="start" height="100%" pt={3}>
               <HiX />
@@ -95,16 +99,36 @@ export default function Hero({ openContact, setOpenContact }) {
 
           <Button
             onClick={() => setOpenContact(!openContact)}
+            pos={[
+              showLogos ? 'absolute' : 'initial',
+              showLogos ? 'absolute' : 'initial',
+              'initial',
+            ]}
+            top={['14px', '14px', 'unset']}
+            left={['15%', '15%', 'unset']}
             colorScheme="blue"
-            pl={showLogos ? '40px' : 50}
-            pr={showLogos ? 1 : 5}
+            pl={[showLogos ? 3 : 5, showLogos ? 3 : 5, showLogos ? '40px' : 50]}
+            pr={[showLogos ? 3 : 5, showLogos ? 3 : 5, showLogos ? 1 : 5]}
+            py={[showLogos ? 1 : 3]}
             zIndex="3"
-            transform="translateX(-2rem)"
+            transform={[
+              'translateY(-30%)',
+              'translateY(-30%)',
+              'translateX(-2rem)',
+            ]}
+            h={[showLogos ? 'auto' : 120, showLogos ? 'auto' : 120, 10]}
           >
-            <Flex gap={3} align="center">
+            <Flex
+              gap={[0, 0, 3]}
+              align={['center', 'center']}
+              justify={['end', 'end', 'center']}
+              h="100%"
+              direction={['column', 'column', 'row']}
+            >
               <HiMail size="2rem" />
               <Text
                 maxW={showLogos ? 0 : 70}
+                maxH={showLogos ? 0 : 70}
                 overflow="hidden"
                 transition="200ms"
               >
@@ -114,22 +138,18 @@ export default function Hero({ openContact, setOpenContact }) {
           </Button>
         </Flex>
 
-        <Heading
-          as="h1"
-          size="4xl"
-          mb={3}
-          pt={showLogos ? 150 : 0}
-          transition="400ms ease-in-out"
-        >
-          Chris Haupt's
-        </Heading>
-        <Heading as="h2" size="2xl" mb={5}>
-          Portfolio
-        </Heading>
-        <Heading as="h3" size="lg" maxW={700} fontWeight="400">
-          A collection of some of my favourite projects created over the years
-          using <b>React</b>.
-        </Heading>
+        <Box px={3} pt={showLogos ? 150 : 50} transition="400ms ease-in-out">
+          <Heading as="h1" size="4xl" mb={3}>
+            Chris Haupt's
+          </Heading>
+          <Heading as="h2" size="2xl" mb={10}>
+            Portfolio
+          </Heading>
+          <Heading as="h3" size="lg" maxW={700} fontWeight="400">
+            A collection of some of my favourite projects created over the years
+            using primarily <b>React</b>.
+          </Heading>
+        </Box>
       </Stack>
     </Box>
   );
